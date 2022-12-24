b37d706a52c6d6552256503b5674de936eba623d
nelsonjcamacaro <nelsonjcamacaro@gmail.com>
2022-12-24 02:40:51 -0300

    splash timer set

--- app/src/main/java/com/example/spotiplus/ui/view/SplashActivity.kt
+++ app/src/main/java/com/example/spotiplus/ui/view/SplashActivity.kt
@@ -3,11 +3,18 @@ package com.example.spotiplus.ui.view
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.navigation.findNavController
+import com.example.spotiplus.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startActivity(Intent(this, MainActivity::class.java))
-        finish()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        },2000)
     }
 }
